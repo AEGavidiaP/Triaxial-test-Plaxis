b0975ba819c0fd36f21638921d6b2fd726bf994b
Andrés Gavidia <98181463+AEGavidiaP@users.noreply.github.com>
2022-03-25 16:02:37 -0300

    Add files via upload

---
+++ Mohr Coulomb/TxCID/Compresion/main.py
@@ -0,0 +1,59 @@
+import TriaxialCode
+
+from plxscripting.easy import *
+
+l_input = 10000
+l_ouput = 10001
+pw = 'ixcMy6nxXr5/3iA%' #Cambiar segun usuario de PLAXIS
+
+s_i, g_i = new_server('localhost', l_input, password=pw)
+s_o, g_o = new_server('localhost', l_ouput, password=s_i.connection._password)
+
+s_i.new()
+
+# Modify model properties
+
+model = {"titulo": "Calibracion del modelo",
+        "comentarios": "Realizar calibracion de TxCID en compresion para modelo Mohr-Coulomb",
+        "unidad_fuerza": "kN", #Trabajar con kiloNewton -> "kN"; trabajar con MegaNewton -> "MN"
+        "unidad_longitud": "m", #Trabajar en milimetro -> "mm"; trabajar en centimetro -> "cm"; trabajar en metro -> "m"; trabajar en kilometro -> "m"
+        "tiempo": "s", #Trabajar en segundos -> "s"; trabajar en minutos -> "min"; trabajar en horas -> "h"; trabajar en dias -> "day"
+        "pesounitario_agua": 9.8, #Tener encuenta las unidades de fuerza y longitud: (unidad_fuerza)/((unidad_longitud)^3)
+        "modelo": "Axisymmetry", #Trabajar en deformaciones planas ->"Planestrain"; trabajar con modelo axisimitrico "Axisymmetry"
+        "nodos": "15noded" #Numero de nodos a trabajas: 6 nodos -> "6noded"; 15 nodos -> "15noded"
+        }
+
+
+#Define size (test specimen)
+
+width = 1 #Specimen width
+ratio = 2 #H/D ratio of the specimen
+consolidation = [100, 200] #Confinements
+
+#Soil properties & assign material
+
+suelo = {"name": "Suelo 1", #Nombre del suelo a evaluar
+         "E": 20000,         #Modulo de elasticidad
+         "nu": 0.3,        #Modulo de Poisson
+         "c": 0,            #Cohesion
+         "fi": 30,          #angulo de friccion
+         "dilatancia": 0   #angulo de dilatancia
+        }
+
+#Document properties        
+TriaxialCode.properties_model(g_i, model)
+
+#Specimen geometry
+probeta_ensayo, medio = TriaxialCode.make_geometry(g_i, width, ratio)
+
+#Constitutive Model
+ensayo = TriaxialCode.soilmaterial_mc_drain(g_i, suelo)
+
+#Assign constitutive model to the specimen
+probeta_ensayo = g_i.setmaterial(ensayo)
+
+#Generate & select mesh points
+punto = TriaxialCode.mesh_point(g_i, g_o, medio)
+
+#Construction phases for the triaxial test
+TriaxialCode.phase_construction(g_i, g_o, punto, suelo, consolidation)
